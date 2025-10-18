6b3be17511faa5e823997c89b43e8c187bc9acc3
seegan <see.gan21@gmail.com>
2025-10-18 23:27:24 +0530

    uupdate

--- apps/postprocess/cert.sh
+++ apps/postprocess/cert.sh
@@ -1,2 +1,2 @@
-sudo certbot certonly --webroot -w /opt/bitnami/apache/apps/dealportal -d portal.techyline.com
+# sudo certbot certonly --webroot -w /opt/bitnami/apache/apps/dealportal -d portal.techyline.com
 # sudo certbot certonly --webroot -w /opt/bitnami/apache/apps/dealbackoffice -d backoffice.techyline.com
